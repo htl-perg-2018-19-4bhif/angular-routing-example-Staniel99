f48b113712d49f7c4b66a42205139d9d7ed369a6
Stefan Daniel <stefan.daniel99@gmx.at>
2019-04-03 16:52:35 +0200

    initial commit

---
+++ routing-example/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokemonsComponent } from './pokemons/pokemons.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonsComponent,
+    PokemonDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
